3d9ee7ad3e251a208b614b6dcd635964e9aeb208
hiccup01 <yectoplasma@icloud.com>
2016-03-09 19:25:02 +1300

    Add transparency support

--- profile.user.js
+++ profile.user.js
@@ -6,12 +6,12 @@
 // @include     https://scratch.mit.edu/*
 // @exclude     https://scratch.mit.edu
 // @exclude     https://scratch.mit.edu/discuss/*
-// @version     3.0
+// @version     3.3
 // @grant       none
 // @updateURL   https://raw.githubusercontent.com/hiccup01/ProfileWizard/master/profile.user.js
 // @icon        http://www.hiccup01.com/img/pw.png
 // ==/UserScript==
-console.log("Running ProfileWizard v3.0");
+console.log("Running ProfileWizard v3.3");
 function updateBodyHeight() {
  if (location.hash === "#editor") {
   document.body.style.height = "";
@@ -33,20 +33,38 @@ if (status == null || status == undefined || status == "undefined") {
 var textarea = status.innerHTML;
 var colour = textarea.substring((textarea.indexOf("§") + 1), (textarea.indexOf("§") + 8));
 var url = textarea.substring((textarea.indexOf("≤") + 1), (textarea.indexOf("≥")));
+var trans;
+if (textarea.indexOf("Ω") == -1) {
+    console.log("No trans value found...");
+    trans = 1;
+} else {
+    trans = textarea.charAt((textarea.indexOf("Ω") + 1));
+    var isTrans = /^\d+$/.test(trans);
+    if (isTrans != true) {
+        trans = 1;
+        console.log("Invalid trans value...");
+    } else {
+     trans = trans / 10;
+    }
+}
 colour.toString();
 url = encodeURI(url);
 var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colour);
+var boxes = document.body.getElementsByClassName("box");
+for (i = 0; i < boxes.length; i++){
+    boxes[i].style.opacity = trans;
+}
 if (isOk  == true || url !== "" || null || "null" || undefined) {
 if (isOk == true) {
-	console.log("Set page colour to: " + colour);
-	if (url !== "" || null || "null" || undefined) {
-		colour = colour.concat(" url(\"", url, "\") repeat");
-	}
+    console.log("Set page colour to: " + colour);
+    if (url !== "" || null || "null" || undefined) {
+        colour = colour.concat(" url(\"", url, "\") repeat");
+    }
 } else {
-	colour = "#FFFFFF url(\"" + url + "\") repeat";
+    colour = "#FFFFFF url(\"" + url + "\") repeat";
 }
 document.body.style.background = colour;
 } else {
-console.log("No colour found or invalid colour.");
+console.log("No colour found or invalid colour...");
 }
 
